4ff22f9c8fc971c52c2aa100c91cc562e6f0e7c2
GyeongminKimGyeongminKim <151486569+GyeongminKimGyeongminKim@users.noreply.github.com>
2024-05-26 04:12:04 +0900

    Feat: 하단 네비게이션 바(액티비티), 홈 화면(프래그먼트), 퍼스널 화면(프래그먼트) 틀 구현

--- app/src/main/java/com/example/mc_jjikdan/MainActivity.kt
+++ app/src/main/java/com/example/mc_jjikdan/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.mc_jjikdan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.mc_jjikdan.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigation
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_calendar -> {
+                    //loadFragment(CalendarFragment())
+                    true
+                }
+                R.id.nav_food_record -> {
+                    //loadFragment(FoodRecordFragment())
+                    true
+                }
+                R.id.nav_personal -> {
+                    loadFragment(PersonalFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    //loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        // 초기 화면 로드: 홈 Fragment
+        loadFragment(HomeFragment())
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
