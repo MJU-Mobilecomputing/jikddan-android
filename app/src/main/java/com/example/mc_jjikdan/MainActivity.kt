6d7b4cf0397228e2d0290e59b9d2184c3748e1b9
남보라 <nambola@nambolaui-MacBookAir.local>
2024-06-09 13:44:28 +0900

    Added new files and modifications to feat/#14/nickname_data_deletion branch

--- app/src/main/java/com/example/mc_jjikdan/MainActivity.kt
+++ app/src/main/java/com/example/mc_jjikdan/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mc_jjikdan
 
+import PersonalFragment
+import ProfileFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
